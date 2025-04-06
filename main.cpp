e43a07b082023d99abc814b6732bc7828bf5fad5
Peterson Paganelli <petersonpaganelli@outlook.com>
2025-04-05 22:37:35 -0300

    [Feat]: Adiciona o monstro com movimentacao aleatoria e mapa escondido

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <cmath>
 
 using namespace std;
 
@@ -19,6 +20,7 @@ struct player{
 
 struct monstro{
     int x, y; //posicao do monstro
+    int character;// Skin do monstro
     int vida; //vida do monstro
     int forca; //forca do monstro
     int level; //level do monstro
@@ -81,6 +83,20 @@ int menu(){
     }
 };
 
+void alterarMatriz(int matriz[][11], int x, int y, int raio, int linhas, int colunas, int m[][11]) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            // Calculando a distância euclidiana do ponto (i, j) até o centro (x, y)
+            double distancia = sqrt(pow(i - x, 2) + pow(j - y, 2));
+            // Se a distância for menor ou igual ao raio, alteramos o valor
+            if (distancia <= raio) {
+                matriz[i][j] = m[i][j];
+            }
+        }
+    }
+}
+
+
 int main()
 {
     ///ALERTA: NAO MODIFICAR O TRECHO DE CODIGO, A SEGUIR.
@@ -114,6 +130,7 @@ int main()
                     1,0,0,0,0,0,0,0,0,0,1,
                     1,1,1,1,1,1,1,1,1,1,1
                 };
+    int M_aux[11][11]={4};
     //Variavel auxiliar geral
     int aux=0;
     //auxiliar para a parede funcionar
@@ -122,6 +139,8 @@ int main()
     int x=5, y=5;
     //Variavel para tecla precionada
     char tecla;
+    // Alterando a matriz com base na coordenada e no raio
+    int raio=3;
     //--------------------------------------------------//
 
     while(aux != 1){
@@ -142,63 +161,109 @@ int main()
     p1.exp = 0;
     ///Criação do personagem - Fim
 
+    /// Criação do monstro - Inicio
+        monstro m1;
+        m1.x = 1;
+        m1.y = 1;
+        m1.character = 'M'; // Caractere do Monstro 'M'
+        m1.vida = 20;
+        m1.forca = 2;
+        m1.level = 4;
+        m1.expToPlayer = 3;
+    /// Criação do monstro - Fim
 
-    while(true){
-        ///Posiciona a escrita no iicio do console
+
+    while (true) {
+        /// Posiciona a escrita no início do console
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 
-        ///Imprime o jogo: mapa e personagem.
-        for(int i=0;i<a;i++){
-            for(int j=0;j<b;j++){
-                if(i==p1.x && j==p1.y){
-                    //if da parede
-                    if(m[i][j]==1){
-                        p1.x=aux1;
-                        p1.y=aux2;
+        // Atualiza a matriz auxiliar com base na posição do jogador e no raio de visibilidade
+        alterarMatriz(M_aux, p1.x, p1.y, raio, 11, 11, m);
+
+        // Verifica os limites do jogador
+        if (p1.x < 0) p1.x = 0;
+        if (p1.x >= a) p1.x = a - 1;
+        if (p1.y < 0) p1.y = 0;
+        if (p1.y >= b) p1.y = b - 1;
+
+        /// Imprime o jogo: mapa e personagem.
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                if (i == p1.x && j == p1.y) {
+                    // Verifica se o jogador está em uma parede
+                    if (M_aux[i][j] == 1) {
+                        p1.x = aux1;
+                        p1.y = aux2;
                     }
-                    cout<<char(p1.character); //personagem
-                    //auxiliares salvando o lugar do personagem
-                    aux1=p1.x; aux2=p1.y;
+                    cout << char(p1.character); // personagem
+                    // Salva a posição atual do jogador
+                    aux1 = p1.x;
+                    aux2 = p1.y;
+                } else if (i == m1.x && j == m1.y) {
+                    // Exibe o monstro
+                    cout << char(m1.character);
+                } else {
+                    // Exibe o mapa com base na matriz auxiliar
+                    switch (M_aux[i][j]) {
+                        case 0: cout << " "; break; // área não descoberta
+                        case 1: cout << char(219); break; // parede
+                        case 2: cout << char(2); break; // porta
+                        default: cout << " "; // área fora do raio de visibilidade
+                    }
                 }
-
-                else{
-                    switch (m[i][j]){
-                        case 0: cout<<"."; break; //caminho não descoberto
-                        case 1: cout<<char(219); break; //parede
-
-                        case 2: cout<<char(2); break; //Porta
-                        //case 3: cout<<":"; break; //Passagem
-                        //default: cout<<"-"; //erro
-                    } //fim switch
-                }
-
             }
+            cout << "\n";
+        } // fim do for do mapa
 
-            cout<<"\n";
-        } //fim for mapa
-
-        ///executa os movimentos
-         if ( _kbhit() ){
+        /// Executa os movimentos
+        if (_kbhit()) {
             tecla = getch();
-            switch(tecla)
-            {
-                case 72: case 'w': ///cima
+            switch (tecla) {
+                case 72: case 'w': /// cima
                     p1.x--;
-                break;
-                case 80: case 's': ///baixo
+                    break;
+                case 80: case 's': /// baixo
                     p1.x++;
-                break;
-                case 75:case 'a': ///esquerda
+                    break;
+                case 75: case 'a': /// esquerda
                     p1.y--;
-                break;
-                case 77: case 'd': ///direita
+                    break;
+                case 77: case 'd': /// direita
                     p1.y++;
-                break;
+                    break;
             }
-         }
 
-
-    } //fim do laco do jogo
+            // Movimentação aleatória do monstro respeitando paredes
+            int novoX, novoY, tent = 0;
+            bool moveu = false;
+            while (tent < 10 && !moveu) {
+                int aux = rand() % 4 + 1;
+                novoX = m1.x;
+                novoY = m1.y;
+                switch (aux) {
+                    case 1: // Cima
+                        novoX--;
+                        break;
+                    case 2: // Baixo
+                        novoX++;
+                        break;
+                    case 3: // Esquerda
+                        novoY--;
+                        break;
+                    case 4: // Direita
+                        novoY++;
+                        break;
+                }
+                // Verifica se o movimento é válido (dentro dos limites e não é parede)
+                if (novoX >= 0 && novoX < a && novoY >= 0 && novoY < b && m[novoX][novoY] != 1) {
+                    m1.x = novoX;
+                    m1.y = novoY;
+                    moveu = true;
+                }
+                tent++;
+            }
+        }
+    } // fim do laço do jogo
 
     return 0;
 } //fim main
